f91b4c72f37c19c7d14a09f27741b4ba911134b8
subbasri17 <subasundaram17@gmail.com>
2026-01-13 15:18:48 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins Pipeline Demo!");
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
